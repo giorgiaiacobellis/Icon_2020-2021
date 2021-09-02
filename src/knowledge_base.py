5fb90bd63ef48e5b8845240958df94723fb7b349
ManuelCitarella <manuel.citarella@libero.it>
2021-09-02 11:32:40 +0200

    Modificata immagine alla relazione

--- src/knowledge_base.py
+++ src/knowledge_base.py
@@ -90,7 +90,7 @@ def askGenereDaTitolo(titolo:str,genere:str):
        print("askGenereDaTitolo("+titolo+","+genere+") <=> "+stringa)
        rispostaUtente=input("Digitare 'how i' specificando al posto di i il numero dell'atomo : ")
        if(rispostaUtente.lower()=='how 1'):
-           print(stringa + " is ", risposta)
+           print(stringa + " <=>", risposta)
        else:
            print("Errore di digitazione")
     else:
@@ -128,40 +128,40 @@ def askStessoGenere(titolo1:str, titolo2:str):
     # Spiega come si è arrivati ai risultati
     rispostaUtente=input("Digitare how per la spiegazione: ")
     if (rispostaUtente.lower()=="how"):
-       print("askStessoGenere("+titolo1+","+titolo2+") <=> "+titolo1+"_"+titolo2+" and  "+titolo1+"_"+primoGenere+ " and "+titolo2+"_"+secondoGenere+" and generiUguali("+primoGenere+","+secondoGenere+")")
+       print("askStessoGenere("+titolo1+","+titolo2+") <=> "+titolo1+"_"+primoGenere+ " and "+titolo2+"_"+secondoGenere+" and generiUguali("+primoGenere+","+secondoGenere+")")
 
        rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
        if(rispostaUtente.lower()=='how 1'):
-           print(titolo1+"_"+primoGenere+" is", risposte.get(1))
+           print(titolo1+"_"+primoGenere+" <=>", risposte.get(1))
            rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
            if(rispostaUtente.lower() =="how 2"):
-               print(titolo2+"_"+secondoGenere+") is ",risposte.get(2))       
+               print(titolo2+"_"+secondoGenere+" <=>",risposte.get(2))       
                rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
                if(rispostaUtente=="how 3"): 
-                   print("generiUguali("+titolo1+","+titolo2+" <=> "+primoGenere+"_"+secondoGenere+" is", risposte[3])       
+                   print("generiUguali("+primoGenere+","+secondoGenere+") <=>", risposte[3])       
                else:
                    print("Errore di digitazione")
            else: 
                print("Errore di digitazione")
        else:
            if(rispostaUtente.lower() =="how 2"):
-               print("SecondaCorrispondenza("+titolo2+") <=> corrispondenzaEsiste("+titolo2+" , " + secondoGenere+ ") is ", risposte.get(2))       
+               print("SecondaCorrispondenza("+titolo2+") <=> corrispondenzaEsiste("+titolo2+" , " + secondoGenere+ ") <=>", risposte.get(2))       
                rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
                
                if(rispostaUtente.lower()=="how 1"):
-                   print("PrimaCorrispondenza("+titolo1+") <=> corrispondenzaEsiste("+titolo1+" , " + primoGenere+ ") is ", risposte.get(1))
+                   print("PrimaCorrispondenza("+titolo1+") <=> corrispondenzaEsiste("+titolo1+" , " + primoGenere+ ") <=>", risposte.get(1))
                else:
                    if(rispostaUtente=="how 3"): 
-                       print("stessoGenere("+titolo1+","+titolo2+") <=> "+primoGenere+"_"+secondoGenere+" is", risposte[3])       
+                       print("stessoGenere("+titolo1+","+titolo2+") <=> "+primoGenere+"_"+secondoGenere+" <=>", risposte[3])       
                    else:
                        print("Errore di digitazione")
                 
            else:
                if(rispostaUtente.lower() =="how 3"):
-                   print("stessoGenere("+titolo1+","+titolo2+") <=>  generiUguali("+primoGenere+","+secondoGenere+") is", risposte[3])
+                   print("stessoGenere("+titolo1+","+titolo2+") <=>  generiUguali("+primoGenere+","+secondoGenere+") <=>", risposte[3])
                    rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
                    if(rispostaUtente.lower() =="how 2"):
-                       print("SecondaCorrispondenza("+titolo2+") <=> corrispondenzaEsiste("+titolo2+" , " + secondoGenere+ ") is ", risposte.get(2))       
+                       print("SecondaCorrispondenza("+titolo2+") <=> corrispondenzaEsiste("+titolo2+" , " + secondoGenere+ ") <=>", risposte.get(2))       
                        rispostaUtente=input("Digitare 'how i' specificando in i il numero dell'atomo per ulteriori informazioni: ")
                         
                        if(rispostaUtente.lower()=="how 1"):
